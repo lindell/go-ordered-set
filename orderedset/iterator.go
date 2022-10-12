33e78d29206ad9adb9fb59260947d2818e3068ea
Johan Lindell <johan@lindell.me>
2022-10-12 08:21:29 +0000

    Updated comments

--- orderedset/iterator.go
+++ orderedset/iterator.go
@@ -1,10 +1,20 @@
+// Package orderedset implements an ordered set, i.e. a set that also keeps track of
+// the order in which values were inserted.
+//
+// All operations are constant-time (except getting a values that are O(n)).
+
 package orderedset
 
+// Iterator is used to iterate over an OrderedSet.
 type Iterator[T comparable] struct {
 	reversed bool
 	current  *listElement[T]
 }
 
+// Next returns the next value in the iteration if there is one,
+// and reports whether the returned value is valid.
+// Once Next returns ok==false, the iteration is over,
+// and all subsequent calls will return ok==false.
 func (i *Iterator[T]) Next() (val T, ok bool) {
 	if i.reversed {
 		current := i.current
